64894acc1ad81389548598b8dfa3efff44507158
kwilki <kylewilkinson14@gmail.com>
2022-08-19 16:07:41 +1000

    commit tree 6f0f741e30c0cf9045e80059a34357be6427fbfc

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+    badges = []
+    names.each{ |name| 
+    badges << "Hello, my name is #{name}."
+    }
+    badges
+end
+
+def assign_rooms(attendees)
+    room_assignment = []
+    attendees.each { |name|
+        index = attendees.index(name) + 1
+        room_assignment << "Hello, #{name}! You'll be assigned to room #{index}!"
+    }
+    room_assignment
+end
+
+def printer(attendees)
+    batch_badge_creator(attendees).each { |badge|
+        puts badge
+    }
+    assign_rooms(attendees).each { |assigned|
+        puts assigned
+    }
+end
